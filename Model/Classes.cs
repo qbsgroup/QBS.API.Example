0b03f08ce32ee1dd4a2f4bf1527ea0729ac1bc20
Mark Brummel <brummelds@gmail.com>
2021-03-03 17:53:47 +0100

    Initial Commit

---
+++ Model/Classes.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QBS.API.Example.Model
+{
+    public class Company
+    {
+        public Guid id { get; set; }
+        public string systemVersion { get; set; }
+        public string name { get; set; }
+        public string displayName { get; set; }
+        public string businessProfileId { get; set; }
+        public DateTime systemCreatedAt { get; set; }
+        public string systemCreatedBy { get; set; }
+        public DateTime systemModifiedAt { get; set; }
+        public string systemModifiedBy { get; set; }
+    }
+
+    public class Customer
+    {
+        public Guid id { get; set; }
+        public string number { get; set; }
+        public string displayName { get; set; }
+        public string type { get; set; }
+        public string addressLine1 { get; set; }
+        public string addressLine2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string postalCode { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public string website { get; set; }
+        public bool taxLiable { get; set; }
+        public Guid taxAreaId { get; set; }
+        public string taxRegistrationNumber { get; set; }
+        public Guid currencyId { get; set; }
+        public string currencyCode { get; set; }
+        public Guid paymentTermsId { get; set; }
+        public Guid shipmentMethodId { get; set; }
+        public Guid paymentMethodId { get; set; }
+        public string blocked { get; set; }
+
+    }
+    public class Credentials
+    {
+        public string UserId { get; set; } = "Your User Here";
+        public string AccessKey { get; set; } = "Your Access Key from Business Central"
+    }
+    public class APIEndpoint
+    {
+        public string tenantId { get; set; } = "your tenant ID here";
+        public string companyId { get; set; } = "your company ID here";
+        public string apiPublisher { get; set; } = "api";
+        public string apiGroup { get; set; } = "";
+        public string apiVersion { get; set; } = "v2.0";
+        public string apiEndpoint { get; set; } = "";
+        public string sandBoxName { get; set; } = "sandbox";
+
+        public String getURI()
+        {
+            String uri = "";
+            if (apiEndpoint == "")
+            {
+                uri = (String.Format("https://api.businesscentral.dynamics.com/v2.0/{0}/{3}/{1}/{2}/companies",
+                    tenantId, apiPublisher, apiVersion, sandBoxName));
+            }
+            else
+            {
+                if (apiGroup == "")
+                    uri = (String.Format("https://api.businesscentral.dynamics.com/v2.0/{0}/{5}/{1}/{2}/companies({3})/{4}",
+                        tenantId, apiPublisher, apiVersion, companyId, apiEndpoint, sandBoxName));
+                else
+                    uri = (String.Format("https://api.businesscentral.dynamics.com/v2.0/{0}/{6}/{1}/{2}/{3}/companies({4})/{5}",
+                    tenantId, apiPublisher, apiGroup, apiVersion, companyId, apiEndpoint, sandBoxName));
+            }
+            return uri;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using QBS.API.Example.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QBS.API.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<Company> companies = ListCompanies();
+          
+            foreach (var company in companies.Select((value, index) => new { value, index }))
+                Console.WriteLine("(" + company.index.ToString() + ") Company found with name " + company.value.name + " and id " + company.value.id.ToString());
+
+            int WhichCompanyToUse = companies.Count;
+
+            ConsoleKeyInfo keypressed = Console.ReadKey();
+            if (char.IsDigit(keypressed.KeyChar))
+                WhichCompanyToUse = int.Parse(keypressed.KeyChar.ToString()); 
+            
+            if (WhichCompanyToUse >= companies.Count)
+                throw new Exception("Not a valid selection");
+
+            Console.WriteLine("Selected Company to read customers  = " + companies[WhichCompanyToUse].name);
+
+            CreateNewCustomer(companies[WhichCompanyToUse].id);
+
+            List<Customer> customers = ListCustomers(companies[WhichCompanyToUse].id);
+            foreach (Customer customer in customers)
+                Console.WriteLine(customer.displayName);
+
+
+            Console.ReadKey();
+        }
+        static List<Company> ListCompanies()
+        {
+            RestSharp.RestClient client = CreateRestClient(GetCompanyParameters());
+
+            RestSharp.RestRequest request = GetRequest(RestSharp.Method.GET, new JObject());
+
+            RestSharp.IRestResponse response = client.Execute(request);
+            List<Company> companies = JsonConvert.DeserializeObject<List<Company>>(ConvertToDataSet(response.Content.ToString()).ToString());
+            return companies;
+        }
+        static List<Customer> ListCustomers(Guid CompanyId)
+        {
+            RestSharp.RestClient client = CreateRestClient(GetCustomerParameters(CompanyId));
+
+            RestSharp.RestRequest request = GetRequest(RestSharp.Method.GET, new JObject());
+
+            RestSharp.IRestResponse response = client.Execute(request);
+            List<Customer> customers = JsonConvert.DeserializeObject<List<Customer>>(ConvertToDataSet(response.Content.ToString()).ToString());
+            return customers;
+        }
+
+        static void CreateNewCustomer(Guid CompanyId)
+        {
+            RestSharp.RestClient client = CreateRestClient(GetCustomerParameters(CompanyId));
+
+            Customer cust = new Customer();
+            cust.displayName = "QBS Group helps you out!";
+
+            JObject json = (JObject)JToken.FromObject(cust);
+            
+            RestSharp.RestRequest request = GetRequest(RestSharp.Method.POST, json);
+
+            RestSharp.IRestResponse response = client.Execute(request);
+
+        }
+
+        private static JArray ConvertToDataSet(string result)
+        {
+            dynamic json = JsonConvert.DeserializeObject(result);
+            _ = new JArray();
+            JArray myObject = json["value"];
+
+            return myObject;
+        }
+
+        private static RestSharp.RestRequest GetRequest(RestSharp.Method method, JObject json)
+        {
+            var request = new RestSharp.RestRequest(method);
+
+            Credentials credentials = new Credentials();
+
+            byte[] UserInfo = System.Text.ASCIIEncoding.ASCII.GetBytes(String.Format("{0}:{1}", credentials.UserId, credentials.AccessKey));
+
+            request.AddHeader("Authorization", "Basic " + Convert.ToBase64String(UserInfo));
+            request.AddHeader("Content-Type", "application/json");
+            if (json.Count > 0)
+                request.AddParameter("application/json", json.ToString(), RestSharp.ParameterType.RequestBody);
+            return request;
+        }
+
+        private static RestSharp.RestClient CreateRestClient(APIEndpoint endpoint)
+        {
+            var client = new RestSharp.RestClient(endpoint.getURI())
+            {
+                Timeout = -1
+            };
+            return client;
+        }
+
+        private static APIEndpoint GetCompanyParameters()
+        {
+            APIEndpoint endpoint = new APIEndpoint();
+            return (endpoint);
+        }
+
+        private static APIEndpoint GetCustomerParameters(Guid CompanyId)
+        {
+            APIEndpoint endpoint = new APIEndpoint();
+            endpoint.companyId = CompanyId.ToString();
+            endpoint.apiEndpoint = "customers";
+            return (endpoint);
+        }
+    }
+}
